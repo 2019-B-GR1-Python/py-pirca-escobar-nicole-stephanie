720059b3c3b6efb793d4df2035c324652d796bb8
nicole-pirca <49648469+nicole-pirca@users.noreply.github.com>
2019-12-09 22:28:33 -0500

    grafica

--- 03-pandas/E_OuputData.py
+++ 03-pandas/E_OuputData.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Dec  3 08:00:08 2019
 
-@author: USRBET
+
+@author: nicole
 """
 
 import pandas as pd
@@ -11,106 +11,94 @@ import os
 import sqlite3
 
 
-path_guardado_bin = "C://Users//USRBET//Documents//GitHub//py-pirca-escobar-nicole-stephanie//03-pandas//Data//artwork_data_completo.pickle"
+path = 'C://Users//NICOLE//Documents//GitHub//py-pirca-escobar-nicole-stephanie//03-pandas//Data//artwork_data.csv'
+df = pd.read_csv(
+        path,
+        nrows = 10)
 
-df5 = pd.read_pickle(path_guardado_bin)
-df= df5.iloc[49980:50019, :].copy()
- ##tipos de archivos
- #JSON
- #EXCEL
- #SQL
- ####   EXCEL 
- 
-df.to_excel('mi_df_completo.xlsx')
-## copiar los indices 
-df.to_excel(path_guardado_bin)
-df.to_excel(path_guardado_bin, index =False)
-columnas =['artist', 'title','year']
-df.to_excel (path_guardado_bin, columns= columnas)
+columnas = ['id','artist','title','medium','year','acquisitionYear','height','width','units']
+df2 = pd.read_csv(
+        path,
+        nrows = 10,usecols=columnas)
 
-## CREAR VARIAS HOJAS DE TRABAJO 
-# 1) SE DEFINE EL PATH
+df3 = pd.read_csv(
+        path,
+        nrows = 10,
+        usecols=columnas,
+        index_col = 'id')
 
-path_multiple = "C://Users//USRBET//Documents//GitHub//py-pirca-escobar-nicole-stephanie//03-pandas//Data//artwork_data.pickle"
-write = pd.ExcelWriter(path_multiple,
-                       engine = 'xlswriter')
+path_save = 'C://Users//NICOLE//Documents//GitHub//py-pirca-escobar-nicole-stephanie//03-pandas//Data//artwork_data.pickle'
+path_save2 = 'C://Users//NICOLE//Documents//GitHub//py-pirca-escobar-nicole-stephanie//03-pandas//Data//whole_artwork_data.pickle'
 
-df.to_excel(writer, sheet_name= 'Primera')
+df3.to_pickle(path_save)
 
-df.to_excel(writer, sheet_name= 'Segunda', index =False)
+df4 = pd.read_csv(path)
+df4.to_pickle(path_save2)
+
+df5 = pd.read_pickle(path_save2)
+
+dfs = pd.read_pickle(path_save2)
+df = dfs.iloc[49980:50019,:].copy()
+
+#ARCHIVOS A LOS QUE PUEDE EXPORTAR UN DATAFRAME
+#JSON
+#EXCEL
+#SQL
 
 
-df.to_excel(writer, sheet_name= 'Tercera', columns = columns)
+### EXCEL  ##
+
+path_guardado = 'C://Users//NICOLE//Documents//GitHub//py-pirca-escobar-nicole-stephanie//03-pandas//Data//mi_df_completo.xlsx'
+df.to_excel(path_guardado)
+df.to_excel(path_guardado, index = False)
+
+columnas = ['artist','title','year']
+df.to_excel(path_guardado, columns = columnas)
+
+### Multiples Hojas de Trabajo ###
+
+path_multiple = 'C://Users//NICOLE//Documents//GitHub//py-pirca-escobar-nicole-stephanie//03-pandas//Data//mi_df_multiples.xlsx'
+writer = pd.ExcelWriter(path_multiple, engine = 'xlsxwriter')
+df.to_excel(writer, sheet_name = 'Primera')
+df.to_excel(writer, sheet_name = 'Segunda', index = False)
+df.to_excel(writer, sheet_name = 'Tercera', columns = columnas)
 
 writer.save()
 
+numero_artistas = df['artist'].value_counts()
+path_colores = 'C://Users//NICOLE//Documents//GitHub//py-pirca-escobar-nicole-stephanie//03-pandas//Data//mi_df_colores.xlsx'
+writer = pd.ExcelWriter(path_colores, engine = 'xlsxwriter')
 
-num_artistas = df['artist'].value_counts() ## agruar por artista 
+numero_artistas.to_excel(writer, sheet_name = 'Artistas')
 
-path_colores = "C://Users//USRBET//Documents//GitHub//py-pirca-escobar-nicole-stephanie//03-pandas//Data//mi_df_colores.xlsx"
-df.to_excel('mi_df_completo_colores.xlsx')
-writer = pd.ExcelWriter(path_colores,
-                       engine = 'xlsxwriter')
-num_artistas.to_excel (writer, sheet_name = 'Artistas')
+hojas_artistas = writer.sheets['Artistas']
 
+rango_celdas = 'B2:B{}'.format(len(numero_artistas.index) + 1)
 
-hoja_artistas= writer.sheets['Artistas']
-
-rango_celdas = 'B2: B{}'.format(len(num_artistas.index) + 1) ## esto pone un 3 +1 que da 4 xd
-
-
-## cear un diccionario
-formato_artistas ={
-        "type": "2_color_scale",
-        "min_value": "10",
+formato_artistas = {
+        "type":"2_color_scale",
+        "min_value":"10",
         "min_type":"percentile",
-        "max_value": "99",
-        "max_type": "percentile"
-        }
-## se crea de que rango a que rago el se define el formato a usar 
-
-hoja_artistas.conditional_format(rango_celdas, formato_artistas)
-
+        "max_value":"99",
+        "max_type":"percentile"}
+hojas_artistas.conditional_format(rango_celdas,formato_artistas)        
 writer.save()
 
+import xlsxwriter
+workbook = xlsxwriter.Workbook('C://Users//NICOLE//Documents//GitHub//py-pirca-escobar-nicole-stephanie//03-pandas//Data//chart_line.xlsx')
+worksheet = workbook.add_worksheet()
 
+# Add the worksheet data to be plotted.
+data = numero_artistas.values
+worksheet.write_column('A1', data)
 
+# Create a new chart object.
+chart = workbook.add_chart({'type': 'line'})
 
+# Add a series to the chart.
+chart.add_series({'values': '=Sheet1!$A$1:$A$6'})
 
+# Insert the chart into the worksheet.
+worksheet.insert_chart('C1', chart)
 
-path_grafico="C://Users//USRBET//Documents//GitHub//py-pirca-escobar-nicole-stephanie//03-pandas//Data//mi_df_grafico.xlsx
-writer = pd.ExcelWriter(path_grafico,
-                       engine = 'xlsxwriter')
-
-
-chart_series ={
-    'values': '=Sheet1!$A$1:$A$6',
-    'marker': {
-        'type': 'square',
-        'size': 8,
-        'border': {'color': 'black'},
-        'fill':   {'color': 'red'},
-    },
-}
-
-
-chart_series.conditional_format(chart_series)
-
-writer.save()
-## el lunes y marte recepcion de proyectos 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+workbook.close()
